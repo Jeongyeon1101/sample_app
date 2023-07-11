7dc2020daa02224b6c9a87f84b65a52641db6ce8
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-07-11 10:47:59 +0000

    [Finish]4-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+  get '/top' => 'homes#top'
+  # コントローラ名#アクション名がurlと同じ場合、urlのみでok(省略できる)
+  # /URLにアクセスすると、↑の記述が実行される。
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
