f9ac36af59c8596320b7edc0aa7b991895452670
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-07-12 15:03:27 +0000

    [Finish]4-12

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,10 +4,14 @@ class ListsController < ApplicationController
   end
 
   def create 
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
-  end 
+    @list = List.new(list_params)
+    if @list.save
+      flash[:notice] = "投稿が成功しました"
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end  
+  end
   
   def index
     @lists = List.all
