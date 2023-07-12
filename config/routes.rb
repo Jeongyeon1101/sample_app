c3ae1517ce27f45a379c1b40a53ebe2e289afb32
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-07-12 12:30:34 +0000

    [Finish]4-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  #createは保存機能を追加するアクション
-  get 'lists' => 'lists#index'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  get '/top' => 'homes#top'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  get 'top' => 'homes#top'
+  resources :lists
   # コントローラ名#アクション名がurlと同じ場合、urlのみでok(省略できる)
   # /URLにアクセスすると、↑の記述が実行される。
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
